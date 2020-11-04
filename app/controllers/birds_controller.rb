c3a0eb22728a32ade5fa8c837604a1181166a3dd
Michael Fahim <michael_fahim@yahoo.com>
2020-11-04 23:13:54 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,9 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+      # render json: birds
+      # render json: birds, only: [:id, :name, :species]
+      render :json => birds, :except => [:created_at, :updated_at]
+    end
   end
 end
